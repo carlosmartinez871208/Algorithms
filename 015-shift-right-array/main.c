38bda9c512b48e9390d9248282ca9c85d3a4eddc
Carlos Martinez <carlos.martinez.871208@gmail.com>
2025-07-06 12:22:05 -0600

    add: hackerrank C exercises.

---
+++ 015-shift-right-array/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int next_permutation(int n, char **s)
+{
+	/**
+	* Complete this method
+	* Return 0 when there is no next permutation and 1 otherwise
+	* Modify array s to its next permutation
+	*/
+    static int itx=1;
+    char* aux;
+    if(itx<=(n-1))
+    {
+        for(int i=0;i<(n-1);i++)
+        {
+            aux=s[i];
+            s[i]=s[n-1];
+            s[n-1]=aux;
+        }
+        itx++;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+	char **s;
+	int n;
+	scanf("%d", &n);
+	s = calloc(n, sizeof(char*));
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = calloc(11, sizeof(char));
+		scanf("%s", s[i]);
+	}
+	do
+	{
+		for (int i = 0; i < n; i++)
+			printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+	} while (next_permutation(n, s));
+	for (int i = 0; i < n; i++)
+		free(s[i]);
+	free(s);
+	return 0;
+}
